35ddbfc81780fbc053a17dc296fe36485af29af7
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-02-04 17:06:30 +0200

    add more properties to the user class

--- src/app/usernames.ts
+++ src/app/usernames.ts
@@ -1,3 +1,3 @@
 export class Usernames {
-  constructor(public id:number,public name:string){}
+  constructor(public id:number,public name:string,public profile_image:string, public followers:number,public following:number){}
 }
--- src/app/usernames/usernames.component.ts
+++ src/app/usernames/usernames.component.ts
@@ -4,6 +4,9 @@ import {Repos} from '../repos'
  import {Usernames} from '../usernames'
  import {UserNamess} from '../UserNamess'
  import {UserService} from '../users/user.service'
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.prod';
+
 @Component({
   selector: 'app-usernames',
   templateUrl: './usernames.component.html',
@@ -16,11 +19,35 @@ users:Usernames[];
 addNewUser(user){
   this.users.push(user)
 }
-  constructor(userSevice:UserService) {
+  constructor(userSevice:UserService,private http:HttpClient) {
     this.users=userSevice.getUser()
    }
 
   ngOnInit() {
+     interface ApiResponse{
+         id:number;
+         name:string
+         avatar_url:string
+         followers:number
+         following:number
+
+    }
+    let promise =new Promise((resolve,reject)=>{
+        this.http.get<ApiResponse>('https://api.github.com/users/' +"Anabella1109"+ '?access_token='+ environment.api_key).toPromise().then(response=>{
+            
+            // this.user.id=response.id
+            // user=new Usernames(response.id,response.name)
+
+            resolve()
+        },
+        error=>{
+                // this.user.id=0;
+                
+                reject(error)
+            }
+        )
+    })
+    return promise
   }
 
 }
