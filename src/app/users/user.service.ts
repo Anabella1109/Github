1f6b7c10e7e1a7c898f899c8a45c0019b7692ff5
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-02-04 16:44:52 +0200

    create servise to get users

---
+++ src/app/users/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Usernames} from '../usernames';
+import {UserNamess} from '../UserNamess'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  getUser(){
+    return UserNamess;
+  }
+
+  constructor() { }
+}
